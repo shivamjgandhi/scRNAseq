d989060a8d6c4aabaacbc6aca06394bd4a2e23de
Shivam Gandhi <shivam.jgandhi@gmail.com>
2021-07-15 18:30:29 -0400

    Revert "first"
    This reverts commit 5d5bb9a5510f023a9745e2cb6003df68850cb1ae.

--- misc.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df1 = pd.read_csv('marker_genes.txt')
-
-df1.to_csv('marker_genes.csv', index=None)
-
--- utils/plotting.py
+++
@@ -1,9 +0,0 @@
-import scanpy as sc
-import matplotlib.pyplot as plt
-import pandas as pd
-from bioinfokit import analys, visuz
-
-
-
-def volcanoPlot(df):
-    visuz.gene_exp.volcano(df=df, lfc='log2FC', pv='p-value')
